dd8f7d2650fa5677004f2c1af940bdcc537563d6
jclee <jclee@meta-verse.co.kr>
2026-02-10 13:39:22 +0900

    BG color change

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="mx-auto mt-auto flex w-full max-w-full items-center px-4 bg-emerald-800 text-sm/[14px] text-white">
+    <footer className="mx-auto mt-auto flex w-full max-w-full items-center px-4 bg-blue-950 text-sm/[14px] text-white">
       <div className="mt-2 flex w-full flex-col items-center justify-center gap-4 py-6">        
         <div className="flex gap-4">
           <a  href="mailto:wocks3254@gamil.com"
